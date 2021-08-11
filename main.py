bcd4b76920b6026214c48da6b4b27a9bdecf2248
Rakeshpatil01 <78411442+Rakeshpatil01@users.noreply.github.com>
2021-08-11 21:04:56 +0530

    finally i found the way to navigate to live clock

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-import datetime
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.screen import MDScreen
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Line
-# from kivy.uix.floatlayout import FloatLayout
+from kivymd.uix.floatlayout import MDFloatLayout
 from math import cos, sin, pi
 from kivy.clock import Clock
+from kivy.properties import NumericProperty
+import datetime
 from kivy.lang import Builder
-# from kivy.animation import Animation
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
-Window.size = (480, 800)
-# from kivy.properties import NumericProperty
-
-
-class MyClockWidget(Screen):
-    pass
+from kivy.properties import ObjectProperty
 
 
 class Ticks(Widget):
@@ -28,44 +24,37 @@ class Ticks(Widget):
         with self.canvas:
             time = datetime.datetime.now()
             Color(0.2, 0.5, 0.2)
-            Line(
-                points=[self.center_x,
-                        self.center_y,
-                        self.center_x + 0.8*self.r *
-                        sin(pi/30 * time.second),
-                        self.center_y+0.8*self.r*cos(pi/30*time.second)],
-                width=1,
-                cap="round")
+            Line(points=[self.center_x, self.center_y, self.center_x+0.8*self.r*sin(pi/30 *
+                 time.second), self.center_y+0.8*self.r*cos(pi/30*time.second)], width=1, cap="round")
             Color(0.3, 0.6, 0.3)
-            Line(
-                points=[self.center_x,
-                        self.center_y,
-                        self.center_x+0.7*self.r*sin(pi/30 * time.minute),
-                        self.center_y+0.7*self.r*cos(pi/30*time.minute)],
-                width=2,
-                cap="round")
+            Line(points=[self.center_x, self.center_y, self.center_x+0.7*self.r*sin(pi/30 *
+                 time.minute), self.center_y+0.7*self.r*cos(pi/30*time.minute)], width=2, cap="round")
             Color(0.4, 0.7, 0.4)
             th = time.hour*60 + time.minute
-            Line(
-                points=[self.center_x,
-                        self.center_y,
-                        self.center_x+0.5*self.r*sin(pi/360*th),
-                        self.center_y+0.5*self.r*cos(pi/360*th)],
-                width=3,
-                cap="round")
+            Line(points=[self.center_x, self.center_y, self.center_x+0.5*self.r*sin(
+                pi/360*th), self.center_y+0.5*self.r*cos(pi/360*th)], width=3, cap="round")
 
 
-Builder.load_file('design.kv')
+class ClockWidget(MDScreen):
+
+    def on_enter(self):
+        Clock.schedule_interval(self.ticks.update_clock, 1)
 
 
-class MyClockApp(MDApp):
+class ContentNavigationDrawer(MDBoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class AIAlarm(MDApp):
     def build(self):
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(MyClockWidget(name="screen_one"))
-        clock = MyClockWidget()
-        Clock.schedule_interval(clock.ticks.update_clock, 1)
-        return clock
+        # theme style
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "LightGreen"
+        # self.theme_cls.primary_hue = "500"
+
+        return Builder.load_file('design.kv')
 
 
 if __name__ == '__main__':
-    MyClockApp().run()
+    AIAlarm().run()
